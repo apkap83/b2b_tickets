4e6837c5df611a777cde829b4cfd4ecf33c36bae
apkap83 <ap.kapetanios@gmail.com>
2024-07-11 17:11:56 +0300

    Add Navbar, Global Theme, Fav Icon, SASS

--- apps/b2b_tickets/app/(allPages)/tickets/page.tsx
+++ apps/b2b_tickets/app/(allPages)/tickets/page.tsx
@@ -1,8 +1,25 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import Stack from '@mui/material/Stack';
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  useTheme,
+  Paper,
+  Container,
+} from '@mui/material';
+import clsx from 'clsx';
+
 import { getAllTickets } from '@/app/lib/actions';
+import ThemeToggle from '@/b2b_tickets_app/components/ThemeToggle';
+import { ColorModeContext, tokens } from '@/b2b_tickets_app/theme';
 
 const App: React.FC = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   useEffect(() => {
     const getTickets = async () => {
       const tickets = await getAllTickets();
@@ -10,7 +27,28 @@ const App: React.FC = () => {
 
     getTickets();
   }, []);
-  return <h1>All Tickets</h1>;
+  return (
+    <Container>
+      <Paper elevation={6}>
+        <Box>
+          <Box sx={{ m: 3, p: 3 }}>
+            <Typography variant="h3" className="text-slate-50">
+              Hello
+            </Typography>
+            <Typography sx={{ mt: 2 }}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reiciendis quaerat a id nostrum! Modi quibusdam delectus,
+              repellat, quidem assumenda aliquam error repudiandae vero itaque
+              facilis alias dolores quis, mollitia voluptatibus!
+            </Typography>
+            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+              Learn more
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
+  );
 };
 
 export default App;
