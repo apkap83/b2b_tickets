558285cf55e00968927ffee8c0bdfcdc91cd7274
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-09-20 18:06:02 +0300

    Fixing Fav Icon

--- libs/assets/src/lib/myAssets/index.ts
+++ libs/assets/src/lib/myAssets/index.ts
@@ -1,7 +1,6 @@
 // Export each asset individually, for example:
 export { default as NovaLogo } from './novaLogo.svg';
 export { default as NovaLogo_49x49 } from './novaLogo_49x49.svg';
-export { default as background_nms_portal } from './background_nms_portal.jpg';
-export { default as background_nms_portal_2 } from './background_nms_portal_2.jpg';
+export { default as background_nms_portal } from './loginBackgroundImage.jpg';
 export { default as united_group_member } from './member-of-united-group.png';
 export { NovaLogoSVG } from './novaLogoSVG';
