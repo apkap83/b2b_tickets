6e8ae527eb1b01dcdfe106687b8b96602ea8336b
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-10-17 18:24:51 +0300

    Added Cookie Banner Consent

---
+++ apps/b2b_tickets/app/CookieConsentBanner.tsx
@@ -0,0 +1,66 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import cookie from 'js-cookie';
+import Button from '@mui/material/Button';
+import { signOut } from 'next-auth/react';
+import config from '@b2b-tickets/config';
+
+const CookieConsentBanner = () => {
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    const consentCookie = cookie.get('cookieConsent');
+
+    if (!consentCookie) {
+      setShowBanner(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    setShowBanner(false);
+    cookie.set('cookieConsent', 'accepted', { expires: 365 });
+  };
+
+  const handleReject = async () => {
+    // setShowBanner(false);
+    cookie.set('cookieConsent', 'rejected', { expires: 365 });
+
+    await signOut({ callbackUrl: process.env.SIGNOUT_CALLBACKURL });
+  };
+
+  if (!showBanner) {
+    return null;
+  }
+
+  if (!config.ShowCookieConsentBanner) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="fixed inset-0 bg-black opacity-40"></div>
+      <div className="w-full fixed bottom-[100px] opacity-100 flex-col justify-center items-center bg-white py-4">
+        <p className="text-center pb-3">
+          This website uses cookies to improve your browsing experience.
+        </p>
+        <p className="text-center pb-3">Please click Accept to continue.</p>
+        <div className="flex justify-center items-center">
+          <Button
+            variant="contained"
+            onClick={handleAccept}
+            sx={{
+              marginRight: '2rem',
+            }}
+          >
+            Accept
+          </Button>
+          <Button variant="outlined" onClick={handleReject}>
+            Reject
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CookieConsentBanner;
