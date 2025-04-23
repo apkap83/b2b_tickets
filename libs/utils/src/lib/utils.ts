1525cc8a8d83c84c79b55adb62313c6557ef4084
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-04-23 16:19:59 +0300

    Add Ability to Login By E-mail and Set E-mail as the User Name in the New User Creation Notification

--- libs/utils/src/lib/utils.ts
+++ libs/utils/src/lib/utils.ts
@@ -406,3 +406,8 @@ export function generateOtp(length: number): string {
     .map((x) => digits[x % 10])
     .join('');
 }
+
+export function isValidEmail(email: string) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
