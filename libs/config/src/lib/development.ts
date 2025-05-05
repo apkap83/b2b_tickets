eb4df31dadce5644f9eca55b8e8bd8ef2bed437a
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-05-05 21:02:48 +0300

    Updated Config Files Without Sensitive Info

---
+++ libs/config/src/lib/development.ts
@@ -0,0 +1,84 @@
+const os = require('os');
+
+const isMacDevelopment =
+  os.hostname() === 'MacBook-Pro-Apostolos.local' ||
+  os.hostname() === 'ipintegrator1' ||
+  os.hostname() === 'macbookpro';
+
+export const developmentConfig = {
+  webSiteUrl: process.env['WEBSITE_URL'] as string,
+
+  emailListOfHandlers: [process.env['EMAIL_LIST_OF_HANDLERS'] as string],
+
+  SMSGateway: 'smpp://test-sms-gw1:5000',
+  SMS_SystemId: 'B2BTicketing',
+  SMS_Password: process.env['SMS_PASSWORD'] as string,
+  SMS_System_Type: 'B2BGW',
+
+  logDirPerEnvironment: 'development',
+  SendEmails: true,
+
+  TimeZone: 'Europe/Athens',
+
+  redisHost: '127.0.0.1',
+  redisPort: 6379,
+
+  postgres_b2b_database: {
+    host: isMacDevelopment ? '127.0.0.1' : 'b2b_database',
+    port: isMacDevelopment ? 5432 : 9002,
+    db: 'postgres',
+    username: 'postgres',
+    schemaName: 'b2btickets_dev',
+    debugMode: false,
+    minConnections: 3,
+    maxConnections: 3,
+    sequelizeMinConnections: 3,
+    sequelizeMaxConnections: 3,
+    applicationName: 'B2B Tickets Development',
+    applicationNameSequelize: 'B2B Tickets Development (Sequelize)',
+    acquire: 45000,
+    idleTimeout: 10000,
+    connectionTimeout: 45000,
+  },
+  api: {
+    process: 'b2btickets_development',
+    user: 'DevelopmentApiUser',
+  },
+  logging: {
+    applicationLoggingLevel: 'info',
+  },
+  TICKET_ITEMS_PER_PAGE: 20,
+  CaptchaIsActive: false,
+  CaptchaIsActiveForPasswordReset: true,
+  CaptchaV3Threshold: 0.1,
+  TwoFactorEnabled: true,
+  TwoFactorEnabledForPasswordReset: true,
+  TwoFactorDigitsLength: 5,
+  TwoFactorValiditySeconds: 270,
+  ShowCookieConsentBanner: true,
+  SessionMaxAge: 60 * 60, // Session max age to 60 minutes (in seconds)
+  SessionUpdateAge: 60, // Session is refreshed every 60 seconds
+
+  SessionExpirationPopupShownInSeconds: 60 * 5, // 5 min
+
+  PasswordComplexityActive: true,
+  MinimumPasswordCharacters: 8,
+
+  EmailRelayServerIP: process.env['EMAIL_RELAY_SERVER_IP'] as string,
+  EmailRelayServerTCPPort: '25',
+  EmailFromAddress: process.env['EMAIL_FROM_ADDRESS'] as string,
+
+  userCreationSecureLinkValidity: '2d', // How many days the secure link of user creation / reset password is valid
+
+  cookieConsentValidityInDays: 365,
+
+  maxOTPAttemps: 3,
+  maxOTPAttemptsBanTimeInSec: 300,
+  totpJWTTokenCookieValidityInSec: 300,
+
+  maxTokenAttempts: 5,
+  maxTokenAttemptsBanTimeInSec: 300,
+  emailJWTTokenCookieValidityInSec: 300,
+
+  databaseIDOfTicketingSystemCompany: -1,
+};
