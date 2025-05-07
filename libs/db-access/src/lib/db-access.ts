a09dabf14d05edf3d6993098daec1c841d76ecfc
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-05-07 18:52:14 +0300

    Update PG Pool option (remove min)

--- libs/db-access/src/lib/db-access.ts
+++ libs/db-access/src/lib/db-access.ts
@@ -10,7 +10,6 @@ export const pgB2Bpool = new Pool({
   host: config.postgres_b2b_database.host,
   port: config.postgres_b2b_database.port,
   database: config.postgres_b2b_database.db,
-  min: config.postgres_b2b_database.minConnections,
   max: config.postgres_b2b_database.maxConnections,
   application_name: config.postgres_b2b_database.applicationName,
   connectionTimeoutMillis: config.postgres_b2b_database.connectionTimeout,
