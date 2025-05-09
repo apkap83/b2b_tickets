83b07a439c5513aab002a2fe5154acfda085c4ca
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-05-09 14:48:33 +0300

    Removing "@/*": ["./*"], from paths of tsconfig.base.json

--- libs/shared-models/src/index.ts
+++ libs/shared-models/src/index.ts
@@ -1,9 +1,1 @@
 export * from './lib/shared-models';
-
-// export {
-//   AuthenticationTypes,
-//   ErrorCode,
-//   TransportName,
-//   AppPermissionType,
-//   AppRoleTypes,
-// } from './lib/shared-models';
