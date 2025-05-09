0d542a2ab795f669bae92d6c2044e741a4a41892
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-05-09 13:09:56 +0300

    Jest Config for Typescript regarding auth-options

---
+++ libs/auth-options/jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  displayName: 'auth-options',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {}], // Ensure ts-jest is transforming .ts files
+    '^.+\\.tsx$': ['ts-jest', {}], // Also ensure .tsx files are handled
+  },
+
+  transformIgnorePatterns: [
+    '/node_modules/(?!your-esm-package|another-esm-package)', // Make sure Jest can process ESM modules
+  ],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Point to the setup file
+
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+};
+
+export default config;
