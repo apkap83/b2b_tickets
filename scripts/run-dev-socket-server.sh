ea5af56cd89887a9ac5bf0c9f90a95f9ba51304d
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-06-01 12:51:09 +0300

    create run-dev-socket-server.sh

---
+++ scripts/run-dev-socket-server.sh
@@ -0,0 +1,6 @@
+export NODE_ENV=development
+export PORT=3455
+export SOCKET_CORS_ORIGIN=http://127.0.0.1:3000
+export NEXT_AUTH_SESSION_URL=http://127.0.0.1:3000/api/auth/session
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+nx serve socket-server 
