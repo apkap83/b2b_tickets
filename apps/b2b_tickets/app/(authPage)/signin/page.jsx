4c7820bf5afe3b6cecfd100dc2e360a19f4575ce
apkap83 <ap.kapetanios@gmail.com>
2025-06-07 14:43:15 +0300

    Avoid SignIn Page Flashing From a newly openned but already authenticated browser

--- apps/b2b_tickets/app/(authPage)/signin/page.jsx
+++ apps/b2b_tickets/app/(authPage)/signin/page.jsx
@@ -1,5 +1,7 @@
 // app/auth/signin/page.jsx
 import { getProviders, getCsrfToken } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { options } from '@b2b-tickets/auth-options';
 import SignInForm from './components/SigninForm';
 import Image from 'next/image';
 
@@ -7,9 +9,20 @@ import { background_nms_portal as imagePath } from '@b2b-tickets/assets';
 import { Footer } from '@b2b-tickets/ui';
 import { ReCaptchaProvider } from 'next-recaptcha-v3';
 
-export default async function SignIn() {
+export default async function SignIn({ searchParams }) {
   const providers = await getProviders();
   const csrfToken = await getCsrfToken();
+
+  // Check if user is already authenticated
+  const session = await getServerSession(authOptions);
+
+  // Get the callback URL from search parameters
+  const callbackUrl = searchParams?.callbackUrl || '/tickets';
+
+  // If authenticated, redirect to the callback URL or tickets
+  if (session) {
+    redirect(callbackUrl);
+  }
 
   return (
     <>
