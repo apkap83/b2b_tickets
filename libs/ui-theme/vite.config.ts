7c194466b6827134e819564d681520b5c429f1c9
apkap83 <33512036+apkap83@users.noreply.github.com>
2025-11-28 20:21:58 +0200

    fix: enable debug mode for nxViteTsPaths in Vite configuration

--- libs/ui-theme/vite.config.ts
+++ libs/ui-theme/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 
   plugins: [
     react(),
-    nxViteTsPaths(),
+    nxViteTsPaths({ debug: false }),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
